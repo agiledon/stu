bbdbc7b0d9fdf264ea04e3ffd7c4661ec02eda79
Zhang Yi <bruce.e.zhang@gmail.com>
2017-04-25 14:50:31 +0800

    support user-agent; refactor get method

--- lib/stuart/http_client/httpoison_client.ex
+++ lib/stuart/http_client/httpoison_client.ex
@@ -2,8 +2,18 @@ defmodule Stuart.HttpClient.HTTPoisonClient do
     alias HTTPoison.Response
 
     def get(url) do
-        with {:ok, response} <- HTTPoison.get(url),
-            %Response{body: body} = response,
+        HTTPoison.get(url)
+        |> parse_response
+    end
+
+    def get(url, user_agent) do
+        HTTPoison.get(url, user_agent)
+        |> parse_response
+    end
+
+    defp parse_response(get_response) do
+        with {:ok, response} <- get_response,
+            %Response{status_code: 200, body: body} = response,
         do: {:ok, body}
     end
 end
--- test/stuart/http_client/httpoison_client_test.exs
+++ test/stuart/http_client/httpoison_client_test.exs
@@ -9,4 +9,10 @@ defmodule Stuart.HttpClient.HTTPoisonClientTest do
     {:ok, body} = HTTPoisonClient.get("http://zhangyi.farbox.com")
     assert String.contains?(body, "<title>张逸</title>")
   end
+
+  test "get zhihu content giving user agent and return 200" do
+    user_agent = [{"User-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"}]
+    {:ok, body} = HTTPoisonClient.get("https://www.zhihu.com", user_agent)
+    assert String.contains?(body, "<title>知乎")
+  end
 end
